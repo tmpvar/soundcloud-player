7383d4af52fac5312af015989cacb9d83dc08271
Elijah Insua <tmpvar@gmail.com>
2011-12-06 19:24:32 -0700

    move sm2 into js

--- js/build.sh
+++ js/build.sh
@@ -1,1 +1,1 @@
-cat jquery.min.js ../sm2/soundmanager2-nodebug-jsmin.js player.js info.js play.js progress.js loading.js volume.js waveform.js overlay.js > build.js
+cat jquery.min.js sm2/soundmanager2-nodebug-jsmin.js player.js info.js play.js progress.js loading.js volume.js waveform.js overlay.js > build.js
